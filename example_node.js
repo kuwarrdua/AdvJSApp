6ed8c5f1c4a770024b0e6102e94a1ee6aa622f72
Kuwar <200430408@student.georgianc.on.ca>
2020-06-04 17:55:18 -0400

    First change

---
+++ example_node.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+
+app.get('/', (request, response) =>{
+    https.get('https://insult.mattbas.org/api/insult', resp => {
+       resp.on('data', chunk => {
+            response.send(`
+                <http>
+                    <head><title>Oh Snap</title></head>
+                    <body style="background-color: #EAE5E4;">
+                    <h1><span style=color:red;">O</span>h <span style=color:red;">S</span>nap</h1>
+                    <p style="font-size: 24px; color:purple; font-weight:bold">
+                        ${chunk.toString()}!
+                    </p>
+                    </body>
+                </http>
+            `);
+       });
+    });
+});
+
+app.listen(3000);   
